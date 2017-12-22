f1b56f2eaa06c2a695cc936005a4f23d84df216e
Martin Bächtold <martin@baechtold.me>
2017-12-22 11:12:38 +0100

    Drop support for Plone 4.2.

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(name='ftw.table',
 
       classifiers=[
           'Framework :: Plone',
-          'Framework :: Plone :: 4.0',
-          'Framework :: Plone :: 4.2',
           'Framework :: Plone :: 4.3',
           'Framework :: Plone :: 5.1',
           'Programming Language :: Python',
